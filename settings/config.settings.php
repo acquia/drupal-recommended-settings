4925e7d5f0f85fa0220f5a5de9e3dcda16fc4f96
Ankit Pathak <106175376+apathak18@users.noreply.github.com>
2024-01-31 06:29:50 +0000

    ACMS-3508: PR feedback work to update the logic.

--- settings/config.settings.php
+++ settings/config.settings.php
@@ -37,11 +37,19 @@ $site_name = EnvironmentDetector::getSiteName($site_path);
 
 // phpcs:ignore
 // Config sync settings.
-$settings['config_sync_directory'] = !$drs_override_config_directories ?
-"../config/$site_name" : "../config/default";
+// if $settings['config_sync_directory'] isn't set by user the DRS
+// set to the ../config/$site_name and if $drs_override_config_directories
+// is TRUE then it sets to ../config/default.
+$config_settings = $settings['config_sync_directory'] ?? "../config/$site_name";
+$settings['config_sync_directory'] = !$drs_override_config_directories ? $config_settings : "../config/default";
+
 // Site Studio sync settings.
-$settings['site_studio_sync'] = !$drs_override_site_studio_sync_directories ?
-"../sitestudio/$site_name" : "../sitestudio/default";
+// if $settings['site_studio_sync'] isn't set by user the DRS
+// set to the ../sitestudio/$site_name and if
+// $drs_override_site_studio_sync_directories is TRUE
+// then it sets to ../sitestudio/default.
+$site_studio_config_settings = $settings['site_studio_sync'] ?? "../sitestudio/$site_name";
+$settings['site_studio_sync'] = !$drs_override_site_studio_sync_directories ? $site_studio_config_settings : "../sitestudio/default";
 
 $split_filename_prefix = 'config_split.config_split';
 
