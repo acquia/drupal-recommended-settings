1c4c233a34fe447079c292ed69c1b58d61998d86
Vishal Khode <vishal.khode@acquia.com>
2024-02-14 15:50:39 +0530

    ACMS-3461: Update PHPUnit tests.

--- tests/src/unit/PluginUnitTest.php
+++ tests/src/unit/PluginUnitTest.php
@@ -3,11 +3,17 @@
 namespace Acquia\Drupal\RecommendedSettings\Tests\unit;
 
 use Acquia\Drupal\RecommendedSettings\Plugin;
+use Acquia\Drupal\RecommendedSettings\Tests\FunctionalBaseTest;
 use Composer\Composer;
+use Composer\Config;
+use Composer\DependencyResolver\Operation\InstallOperation;
 use Composer\DependencyResolver\Operation\OperationInterface;
+use Composer\Installer\PackageEvent;
 use Composer\IO\IOInterface;
+use Composer\Package\RootPackage;
+use Composer\Repository\RepositoryInterface;
 
-class PluginUnitTest extends DrsPhpUnitBase {
+class PluginUnitTest extends FunctionalBaseTest {
 
   /**
    * The recommended setting's plugin object.
@@ -28,41 +34,53 @@ class PluginUnitTest extends DrsPhpUnitBase {
    * Set up test environmemt.
    */
   public function setUp(): void {
+    $config = new Config(TRUE, $this->getProjectRoot());
+    $this->composer = new Composer();
+    $package = new RootPackage("acquia/drupal-recommended-settings", "1.0", "1.0.0");
+    $package->setExtra([
+      "drupal-scaffold" => [
+        "locations" => [
+          "web-root" => "docroot"
+        ],
+      ],
+    ]);
+    $this->composer->setPackage($package);
+    $this->composer->setConfig($config);
     $this->plugin = new Plugin();
-    $this->setClass($this->plugin);
-    $this->composer = $this->createMock(Composer::class);
     $this->io = $this->createMock(IOInterface::class);
-    $this->getClassMethod("activate")->invokeArgs($this->plugin, [$this->composer, $this->io]);
+    $method = $this->getReflectionMethod(Plugin::class, "activate");
+    $method->invokeArgs($this->plugin, [$this->composer, $this->io]);
   }
 
   /**
    * Test to get project root.
    */
   public function testGetProjectRoot(): void {
-    $getProjectRootMethod = $this->getClassMethod("getProjectRoot");
+    $getProjectRootMethod = $this->getReflectionMethod(Plugin::class, "getProjectRoot");
     $projectRootPath = $getProjectRootMethod->invoke($this->plugin);
-    $this->assertIsString($projectRootPath);
+    $this->assertEquals($this->getProjectRoot(), $projectRootPath);
   }
 
   /**
    * Test to get drupal root.
    */
   public function testGetDrupalRoot(): void {
-    $getDrupalRootMethod = $this->getClassMethod("getDrupalRoot");
+    $getDrupalRootMethod = $this->getReflectionMethod(Plugin::class,"getDrupalRoot");
     $drupalRootPath = $getDrupalRootMethod->invoke($this->plugin);
-    $this->assertIsString($drupalRootPath);
+    $this->assertEquals($this->getProjectRoot() . "/docroot", $drupalRootPath);
   }
 
   /**
    * Test to get settings package.
    */
   public function testGetSettingsPackage(): void {
-    $getSettingsPackageMethod = $this->getClassMethod("getSettingsPackage");
-    $operationInterfaceParam = $this->createMock(OperationInterface::class);
-    $getSettingsPackage = $getSettingsPackageMethod->invokeArgs($this->plugin, [$operationInterfaceParam]);
-    // @todo to perform other assertions as this method
-    // invoked on the basis of operation i.e update or install.
-    $this->assertNull($getSettingsPackage);
+    $repository = $this->createMock(RepositoryInterface::class);
+    $operation = new InstallOperation($this->composer->getPackage());
+    $package_event = new PackageEvent("install", $this->composer, $this->io, FALSE, $repository, [], $operation);
+    $this->plugin->onPostPackageEvent($package_event);
+    $getSettingsPackageMethod = $this->getReflectionMethod(Plugin::class,"getSettingsPackage");
+    $getSettingsPackage = $getSettingsPackageMethod->invokeArgs($this->plugin, [$operation]);
+    $this->assertEquals($getSettingsPackage, "acquia/drupal-recommended-settings-1.0");
   }
 
 }
