0a40596b9fd7b48d258b948059822deecfa90a3e
rajeshreeputra <pravin.gaikwad@acquia.com>
2024-09-13 13:39:36 +0530

    ACMS-4134: Update workflow according to Drupal 11.

--- settings/acquia-recommended.settings.php
+++ settings/acquia-recommended.settings.php
@@ -78,6 +78,8 @@ $settings_files = [];
 
 // Add mysql57 backport module.
 if (file_exists(DRUPAL_ROOT . '/modules/contrib/mysql57/settings.inc')) {
+  global $conf, $databases;
+  $conf['acquia_hosting_settings_autoconnect'] = FALSE;
   require DRUPAL_ROOT . '/modules/contrib/mysql57/settings.inc';
 }
 
