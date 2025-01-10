56adadb8f19b00396ea8aa682f10bbd15285b46d
balsama <adam.balsam@acquia.com>
2025-01-10 14:39:46 -0500

    ACMS-4134: What happens if we add it in both places 🤔

--- settings/acquia-recommended.settings.php
+++ settings/acquia-recommended.settings.php
@@ -76,6 +76,11 @@ if ($ip) {
 
 $settings_files = [];
 
+// Add settings file for mysql57 backport module if present.
+if (file_exists(DRUPAL_ROOT . '/modules/contrib/mysql57/settings.inc')) {
+  require DRUPAL_ROOT . '/modules/contrib/mysql57/settings.inc';
+}
+
 /**
  * Site path.
  *
