3d979532387da225958b57b618659a731aa95fd3
“Chandan <chandan.singh@acquia.com>
2025-06-25 14:27:57 +0530

    Update logic to set appropriate sync directory path.

--- settings/config.settings.php
+++ settings/config.settings.php
@@ -15,16 +15,17 @@ use Acquia\Drupal\RecommendedSettings\Helpers\EnvironmentDetector;
  * directory should be shared between all multi-sites, and each multisite will
  * override this selectively using configuration splits. However, some
  * applications may prefer to manage the configuration for each multisite
- * completely separately. If this is the case,
- * set FALSE to $drs_override_config_directories
- * and $drs_override_site_studio_sync_directories
+ * completely separately. If this is the case, set
+ * $drs_override_config_directories & $drs_override_site_studio_sync_directories
+ * to FALSE and along with value for config & site studio sync directory in
+ * settings.php as follows:
  * $settings['config_sync_directory'] = $dir . "/config/$site_dir"
- * $settings['site_studio_sync'] =  $dir . "/sitestudio/$site_dir" in
- * settings.php, and we will not overwrite it.
+ * $settings['site_studio_sync'] =  $dir . "/sitestudio/$site_dir"
+ * then it will not overwrite it.
  */
 // phpcs:ignore
-$drs_override_config_directories = $drs_override_config_directories ?? FALSE;
-$drs_override_site_studio_sync_directories = $drs_override_site_studio_sync_directories ?? FALSE;
+$drs_override_config_directories = $drs_override_config_directories ?? TRUE;
+$drs_override_site_studio_sync_directories = $drs_override_site_studio_sync_directories ?? TRUE;
 
 /**
  * Site path.
@@ -36,20 +37,40 @@ $drs_override_site_studio_sync_directories = $drs_override_site_studio_sync_dire
 $site_name = EnvironmentDetector::getSiteName($site_path);
 
 // phpcs:ignore
+// If $drs_override_config_directories is set to FALSE and user has specified
+// the value of $overridden_config_sync_dir then set config sync directory as
+// value of $overridden_config_sync_dir. If user has only set
+// $drs_override_config_directories is set to FALSE and not specified the value
+// of $overridden_config_sync_dir then it will fallback to the settings file
+// provided by FilePaths::ahSettingsFile().
+if (!$drs_override_config_directories) {
+  if (isset($overridden_config_sync_dir)) {
+    $settings['config_sync_directory'] = $overridden_config_sync_dir;
+  }
+}
+
+// phpcs:ignore
 // Config sync settings.
-// if $settings['config_sync_directory'] isn't set by user the DRS
-// set to the ../config/$site_name and if $drs_override_config_directories
-// is TRUE then it sets to ../config/default.
-$config_settings = $settings['config_sync_directory'] ?? "../config/$site_name";
-$settings['config_sync_directory'] = !$drs_override_config_directories ? $config_settings : "../config/default";
+// if $drs_override_config_directories is TRUE or not specified at all
+// then set sync directory to ../config/default.
+else {
+  $settings['config_sync_directory'] = "../config/default";
+}
 
 // Site Studio sync settings.
-// if $settings['site_studio_sync'] isn't set by user the DRS
-// set to the ../sitestudio/$site_name and if
-// $drs_override_site_studio_sync_directories is TRUE
-// then it sets to ../sitestudio/default.
-$site_studio_config_settings = $settings['site_studio_sync'] ?? "../sitestudio/$site_name";
-$settings['site_studio_sync'] = !$drs_override_site_studio_sync_directories ? $site_studio_config_settings : "../sitestudio/default";
+// if $settings['site_studio_sync'] isn't set by users, then the DRS set it to
+// "../sitestudio/default" and if $drs_override_site_studio_sync_directories is
+// set to FALSE then user needs to provide the value of
+// $settings['site_studio_sync'] =  "../sitestudio/$site_dir" in settings file;
+if ($drs_override_site_studio_sync_directories) {
+  $settings['site_studio_sync'] = "../sitestudio/default";
+} else {
+  if (isset($overridden_site_studio_sync_dir)) {
+    $settings['site_studio_sync'] = $overridden_site_studio_sync_dir;
+  } else {
+    $settings['site_studio_sync'] = "../sitestudio/$site_name";
+  }
+}
 
 $split_filename_prefix = 'config_split.config_split';
 
