fd46af591727de5cbafffea4bc9732272635968f
Vishal Khode <vishal.khode@acquia.com>
2026-02-17 16:14:34 +0530

    Added test coverage for settings.php.

---
+++ tests/src/Mock/Drupal.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Acquia\Drupal\RecommendedSettings\Tests\Mock;
+
+/**
+ * A mock class to provide a Drupal core version for testing purpose.
+ */
+class Drupal {
+
+  /**
+   * The current system version.
+   */
+  const VERSION = '11.x';
+
+}
